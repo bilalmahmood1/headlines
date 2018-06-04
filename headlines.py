211e7d8c22b421517a1e137fa2b7305108902c4b
Bilal Mahmood <bmahmood328@gmail.com>
2018-06-04 05:02:06 +0500

    feeds from multiple sources with simple error handling added

--- headlines.py
+++ headlines.py
@@ -25,27 +25,34 @@ def route(channel = 'bbc'):
 
 def get_news(channel):
     rss_feed = feedparser.parse(RSS_FEEDS.get(channel))
-  
-    entry_html_block = """
-         <b>{0}</b><br/> 
-         <i>{1}</i><br/>
-         <p>{2}</p><br/>        
-    """
-    content = ""  
-    for entry in rss_feed['entries']:
-        content += entry_html_block.format(entry.get('title'),
-                            entry.get('published'),
-                            entry.get('summary'))
-    
-    html_page = """<html>
+    ## A really simple way to handle internet breakdown or wrong URLS
+    if len(rss_feed.entries) == 0 or rss_feed.status != 200:
+        return """<html>
                      <body>
-                     <h1>{0} Headlines </h1>
-                     {1}
+                     <h1>Can't Access Feeds Right Now!</h1>
                     </body>
                   </html>"""
-                  
-    return html_page.format(channel.upper(), content)
-    
+    else:              
+        entry_html_block = """
+             <b>{0}</b><br/> 
+             <i>{1}</i><br/>
+             <p>{2}</p><br/>        
+        """
+        content = ""  
+        for entry in rss_feed['entries']:
+            content += entry_html_block.format(entry.get('title'),
+                                entry.get('published'),
+                                entry.get('summary'))
+        
+        html_page = """<html>
+                         <body>
+                         <h1>{0} Headlines </h1>
+                         {1}
+                        </body>
+                      </html>"""
+                      
+        return html_page.format(channel.upper(), content)
+        
 
 if __name__ == "__main__":
     app.run(host = 'localhost',
